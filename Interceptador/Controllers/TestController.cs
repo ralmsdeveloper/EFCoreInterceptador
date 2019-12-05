ac054b3648300e97dd2632a13ecf1a610e358a9e
Rafael Almeida <ralms@ralms.net>
2019-12-05 00:16:42 -0300

    Small Adjustments

---
+++ Interceptador/Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Interceptador.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        private readonly RalmsContext _db;
+
+        public TestController( RalmsContext sampleContext)
+        {
+            _db = sampleContext;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            _ = _db.People.Count();
+            _ = _db.People.ToList();
+
+            return Ok();
+        }
+    }
+}
