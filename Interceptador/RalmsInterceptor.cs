0f6b595fdb90cd78550fb561d128b9631e21f2b9
Rafael Almeida <ralms@ralms.net>
2019-12-05 00:00:29 -0300

    Add project files.

---
+++ Interceptador/RalmsInterceptor.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using System.Data.Common;
+using System.Text.RegularExpressions;
+
+namespace Interceptador
+{
+    public class RalmsInterceptor : DbCommandInterceptor
+    {
+        private static readonly Regex _tableAliasRegex =
+            new Regex(@"(?<tableAlias>FROM +(\[.*\]\.)?(\[.*\]) AS (\[.*\])(?! WITH \(NOLOCK\)))",
+                RegexOptions.Multiline |
+                RegexOptions.IgnoreCase |
+                RegexOptions.Compiled);
+
+        public override InterceptionResult<DbDataReader> ReaderExecuting(
+            DbCommand command,
+            CommandEventData eventData,
+            InterceptionResult<DbDataReader> result)
+        {
+            if (!command.CommandText.Contains("WITH (NOLOCK)"))
+            {
+                command.CommandText =
+                    _tableAliasRegex.Replace(command.CommandText,
+                    "${tableAlias} WITH (NOLOCK)");
+            }
+
+            return result;
+        }
+    }
+
+    public class RalmsContext : DbContext
+    {
+        public DbSet<Person> People { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=EFCORE31;Trusted_Connection=True;Connect Timeout=5;ConnectRetryCount=0")
+                .AddInterceptors(new RalmsInterceptor());
+        }
+    }
+
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ Interceptador/Startup.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Interceptador
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddDbContext<RalmsContext>();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            //using (var db = app.ApplicationServices.CreateScope().ServiceProvider.GetService<RalmsContext>())
+            //{
+            //    db.Database.EnsureCreated();
+            //}
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
